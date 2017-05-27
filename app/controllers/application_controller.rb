6e17df9a5145a7421fdeb526cc6ad96e0f39192e
Aaron Bartholomew <abartholomew@blixhost.com>
2017-05-27 00:09:30 -0700

    Tracking and new logo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :redirect_subdomain
 
   def feed
     @jobs = JobListing.where(active: 1).order({ created_at: :desc }).limit(25)
     respond_to do |format|
       format.rss { render :layout => false }
+    end
+  end
+
+  def redirect_subdomain
+    if request.host == 'www.remotedigitaljobs.com'
+      redirect_to 'https://remotedigitaljobs.com' + request.fullpath, :status => 301
     end
   end
 
