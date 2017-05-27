3095b51ad83f937c9189b6bcf2d7c788ca7a4536
Aaron Bartholomew <abartholomew@blixhost.com>
2017-05-26 21:39:13 -0700

    .

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,16 @@
 //= require turbolinks
 //= require_tree ./theme
 
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 $('.apply-modal').magnificPopup({
   type: 'inline',
 
@@ -18,3 +28,18 @@ $('.apply-modal').magnificPopup({
   removalDelay: 300,
   mainClass: 'my-mfp-zoom-in'
 });
+
+if($("span.countdown").length > 0) {
+  var timeLeft = parseInt($("span.countdown").text());
+  var endTime = 0;
+  var interval = setInterval(function(){
+    timeLeft--;
+    if(timeLeft==0) {
+      clearInterval(interval);
+      $("span.countdown").text(timeLeft);
+      window.location = getParameterByName('url');
+    } else {
+      $("span.countdown").text(timeLeft);
+    }
+  }, 1000);
+}
