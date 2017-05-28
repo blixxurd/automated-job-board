635227039590a9a71e5143f6baa1779810ff6ecc
Aaron Bartholomew <abartholomew@blixhost.com>
2017-05-27 17:58:29 -0700

    Updates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
 
   def index
-    @jobs = JobListing.where(active: 1).order({ created_at: :desc }).limit(25)
+    @page = (params[:page] && params[:page]).to_i > 0 ? params[:page].to_i : 1
+    @per_page = 25
+    @offset = (@page-1)*@per_page
+    @jobs = JobListing.where(active: 1).offset(@offset).order({ created_at: :desc }).limit(@per_page)
   end
 
   def listing
